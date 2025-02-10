6974c16ba5137f6d8a95e9fae69528f3ca14dc32
JustGoodkoff <justgoodkoff@yandex.ru>
2025-02-10 14:47:04 +0700

    First commit, added CLI parser

---
+++ src/main/java/org/example/CLIParser.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.apache.commons.*;
+import org.apache.commons.cli.*;
+
+import java.text.ParseException;
+import java.util.List;
+
+public class CLIParser {
+    public static CommandLineArguments parse(String[] args) throws ParseException, org.apache.commons.cli.ParseException {
+        Options options = new Options();
+        options.addOption(Option.builder("o").hasArg().argName("path").desc("Output directory").build());
+        options.addOption(Option.builder("p").hasArg().argName("prefix").desc("File prefix").build());
+        options.addOption(Option.builder("a").desc("Append mode").build());
+        options.addOption(Option.builder("s").desc("Short statistics").build());
+        options.addOption(Option.builder("f").desc("Full statistics").build());
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        boolean isShort = cmd.hasOption("s");
+        boolean isFull = cmd.hasOption("f");
+        if (isShort && isFull) {
+            throw new ParseException("Both -s and -f options cannot be specified together", 1);
+        }
+        if (!isShort && !isFull) {
+            throw new ParseException("Either -s or -f option must be specified", 1);
+        }
+
+        StatisticsType statsType = isShort ? StatisticsType.SHORT : StatisticsType.FULL;
+
+        String outputPath = cmd.getOptionValue("o", ".");
+        String prefix = cmd.getOptionValue("p", "");
+        boolean appendMode = cmd.hasOption("a");
+
+        List<String> inputFiles = cmd.getArgList();
+        if (inputFiles.isEmpty()) {
+            throw new ParseException("No input files specified", 1);
+        }
+
+        return new CommandLineArguments(outputPath, prefix, appendMode, statsType, inputFiles);
+    }
+}
